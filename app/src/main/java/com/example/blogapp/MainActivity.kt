9088301d262e314953f7f56d8ebefd1765b2ea6b
Michael Villarruel <alejovillarruel@gmail.com>
2021-09-25 19:40:45 -0500

    get and set data

--- app/src/main/java/com/example/blogapp/MainActivity.kt
+++ app/src/main/java/com/example/blogapp/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.blogapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = FirebaseFirestore.getInstance()
+
+        // Get data
+        db.collection("cities").document("LA").get().addOnSuccessListener { document ->
+            document.let {
+                Log.d("Firebase", "DocumentSnapshot data: ${document.data}")
+                val city = document.toObject(City::class.java)
+                val color = document.getString("color")
+                val population = document.getLong("population")
+                Log.d("Firebase", "Population: $population / Color: $color")
+            }
+        }.addOnFailureListener { error ->
+            Log.e("FirebaseError", error.toString())
+        }
+
+        // Set data
+        db.collection("cities").document("NY").set(City(3000, "red")).addOnSuccessListener {
+            Log.d("Firebase", "City saved")
+        }.addOnFailureListener { error ->
+            Log.e("FirebaseError", error.toString())
+        }
+
     }
 }
+
+data class City(val population: Int = 0, val color: String = "")
