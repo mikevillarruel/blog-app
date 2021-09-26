2d6db9432ed3caf4b2cac35c36e632985b63fa1b
Michael Villarruel <alejovillarruel@gmail.com>
2021-09-25 23:31:29 -0500

    upload photos to cloud storage

--- app/src/main/java/com/example/blogapp/MainActivity.kt
+++ app/src/main/java/com/example/blogapp/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.example.blogapp
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+    private val REQUEST_IMAGE_CAPTURE = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val db = FirebaseFirestore.getInstance()
+        val btnTakePicture = findViewById<Button>(R.id.btnTakePicture)
+        imageView = findViewById(R.id.imageView)
+
+        btnTakePicture.setOnClickListener {
+            dispatchTakePictureIntent()
+        }
+
+        /*val db = FirebaseFirestore.getInstance()
 
         // Get data
         db.collection("cities").document("LA").addSnapshotListener { snapshot, error ->
@@ -28,9 +47,49 @@ class MainActivity : AppCompatActivity() {
             Log.d("Firebase", "City saved")
         }.addOnFailureListener { error ->
             Log.e("FirebaseError", error.toString())
-        }
+        }*/
 
+    }
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(this, "Camera not found", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmpap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmpap)
+            uploadPicture(imageBitmpap)
+        }
+    }
+
+    private fun uploadPicture(bitmap: Bitmap) {
+        val storageRef = FirebaseStorage.getInstance().reference
+        val imageRef = storageRef.child("image.jpg")
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+        val uploadTask = imageRef.putBytes(data)
+        uploadTask.continueWithTask { task ->
+            if (!task.isSuccessful) {
+                task.exception?.let { exception ->
+                    throw exception
+                }
+            }
+            imageRef.downloadUrl
+        }.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val downloadUrl = task.result.toString()
+                Log.d("Storage", "uploadPicture: $downloadUrl")
+            }
+        }
     }
 }
 
-data class City(val population: Int = 0, val color: String = "")
+//data class City(val population: Int = 0, val color: String = "")
