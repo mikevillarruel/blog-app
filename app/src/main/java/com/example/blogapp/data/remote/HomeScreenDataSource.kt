52b644d0d0ee71a2d078f85e811d6e8bcda26d7f
Michael Villarruel <alejovillarruel@gmail.com>
2021-09-28 00:34:32 -0500

    viewmodel, datasource and google services added

---
+++ app/src/main/java/com/example/blogapp/data/remote/HomeScreenDataSource.kt
@@ -0,0 +1,21 @@
+package com.example.blogapp.data.remote
+
+import com.example.blogapp.core.Resource
+import com.example.blogapp.data.model.Post
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
+class HomeScreenDataSource {
+    suspend fun getLatestPosts(): Resource<List<Post>> {
+        val postList = mutableListOf<Post>()
+        val querySnapshot = FirebaseFirestore.getInstance().collection("posts").get().await()
+
+        querySnapshot.documents.forEach { post ->
+            post.toObject(Post::class.java)?.let { newPost ->
+                postList.add(newPost)
+            }
+        }
+
+        return Resource.Success(postList)
+    }
+}
--- app/src/main/java/com/example/blogapp/ui/home/HomeScreenFragment.kt
+++ app/src/main/java/com/example/blogapp/ui/home/HomeScreenFragment.kt
@@ -2,43 +2,55 @@ package com.example.blogapp.ui.home
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.example.blogapp.R
-import com.example.blogapp.data.model.Post
+import com.example.blogapp.core.Resource
+import com.example.blogapp.data.remote.HomeScreenDataSource
 import com.example.blogapp.databinding.FragmentHomeScreenBinding
+import com.example.blogapp.domain.HomeScreenRepoImpl
+import com.example.blogapp.presentation.HomeScreenViewModel
+import com.example.blogapp.presentation.HomeScreenViewModelFactory
 import com.example.blogapp.ui.home.adapter.HomeScreenAdapter
-import com.google.firebase.Timestamp
 
 class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
 
     private lateinit var binding: FragmentHomeScreenBinding
+    private val viewModel by viewModels<HomeScreenViewModel> {
+        HomeScreenViewModelFactory(
+            HomeScreenRepoImpl(
+                HomeScreenDataSource()
+            )
+        )
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentHomeScreenBinding.bind(view)
 
-        val postList = listOf<Post>(
-            Post(
-                "https://as2.ftcdn.net/v2/jpg/02/15/84/43/500_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg",
-                "Jane Doe",
-                Timestamp.now(),
-                "https://as2.ftcdn.net/v2/jpg/02/15/84/43/500_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg"
-            ),
-            Post(
-                "https://as2.ftcdn.net/v2/jpg/02/15/84/43/500_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg",
-                "Jane Doe",
-                Timestamp.now(),
-                "https://as2.ftcdn.net/v2/jpg/02/15/84/43/500_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg"
-            ),
-            Post(
-                "https://as2.ftcdn.net/v2/jpg/02/15/84/43/500_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg",
-                "Jane Doe",
-                Timestamp.now(),
-                "https://as2.ftcdn.net/v2/jpg/02/15/84/43/500_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg"
-            )
-        )
+        viewModel.fetchLatestPosts().observe(viewLifecycleOwner, Observer { result ->
+            when (result) {
+                is Resource.Loading -> {
+                    binding.progressBar.visibility = View.VISIBLE
+                }
+                is Resource.Success -> {
+                    binding.progressBar.visibility = View.GONE
+                    binding.rvHome.adapter = HomeScreenAdapter(result.data)
+                }
+                is Resource.Failure -> {
+                    binding.progressBar.visibility = View.GONE
+                    Toast.makeText(
+                        requireContext(),
+                        "Error: ${result.exception}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        })
 
-        binding.rvHome.adapter = HomeScreenAdapter(postList)
+
     }
 
 }
