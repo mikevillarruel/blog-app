6d646a2484cfeb186071b0a314ef373d04b09cae
Michael Villarruel <alejovillarruel@gmail.com>
2021-09-27 22:09:05 -0500

    set adapter in recycler view and add home screen fragment in main graph

--- app/src/main/java/com/example/blogapp/ui/home/HomeScreenFragment.kt
+++ app/src/main/java/com/example/blogapp/ui/home/HomeScreenFragment.kt
@@ -1,16 +1,44 @@
 package com.example.blogapp.ui.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.blogapp.R
+import com.example.blogapp.data.model.Post
+import com.example.blogapp.databinding.FragmentHomeScreenBinding
+import com.example.blogapp.ui.home.adapter.HomeScreenAdapter
+import com.google.firebase.Timestamp
 
 class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
 
+    private lateinit var binding: FragmentHomeScreenBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeScreenBinding.bind(view)
+
+        val postList = listOf<Post>(
+            Post(
+                "https://as2.ftcdn.net/v2/jpg/02/15/84/43/500_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg",
+                "Jane Doe",
+                Timestamp.now(),
+                "https://as2.ftcdn.net/v2/jpg/02/15/84/43/500_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg"
+            ),
+            Post(
+                "https://as2.ftcdn.net/v2/jpg/02/15/84/43/500_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg",
+                "Jane Doe",
+                Timestamp.now(),
+                "https://as2.ftcdn.net/v2/jpg/02/15/84/43/500_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg"
+            ),
+            Post(
+                "https://as2.ftcdn.net/v2/jpg/02/15/84/43/500_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg",
+                "Jane Doe",
+                Timestamp.now(),
+                "https://as2.ftcdn.net/v2/jpg/02/15/84/43/500_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg"
+            )
+        )
+
+        binding.rvHome.adapter = HomeScreenAdapter(postList)
     }
 
 }
