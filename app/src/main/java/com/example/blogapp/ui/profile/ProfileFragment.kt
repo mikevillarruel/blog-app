65c7e7df96620c5e44d665dd177ffc101bcd34b2
Michael Villarruel <alejovillarruel@gmail.com>
2021-09-29 20:20:47 -0500

    bottom navigation

---
+++ app/src/main/java/com/example/blogapp/ui/profile/ProfileFragment.kt
@@ -0,0 +1,14 @@
+package com.example.blogapp.ui.profile
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.blogapp.R
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
