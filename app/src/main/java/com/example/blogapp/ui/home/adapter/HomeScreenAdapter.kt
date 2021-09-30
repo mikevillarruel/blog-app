928e84b51ffd3bf3fdb2a3b09a2d8863726800a5
Michael Villarruel <alejovillarruel@gmail.com>
2021-09-29 20:56:46 -0500

    add description

--- app/src/main/java/com/example/blogapp/ui/home/adapter/HomeScreenAdapter.kt
+++ app/src/main/java/com/example/blogapp/ui/home/adapter/HomeScreenAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.blogapp.core.BaseViewHolder
+import com.example.blogapp.core.hide
 import com.example.blogapp.data.model.Post
 import com.example.blogapp.databinding.PostItemViewBinding
 
@@ -13,7 +14,8 @@ class HomeScreenAdapter(private val postList: List<Post>) :
     RecyclerView.Adapter<BaseViewHolder<*>>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
-        val itemBinding = PostItemViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val itemBinding =
+            PostItemViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return HomeScreenViewHolder(itemBinding, parent.context)
     }
 
@@ -33,6 +35,11 @@ class HomeScreenAdapter(private val postList: List<Post>) :
             Glide.with(context).load(item.postImage).centerCrop().into(binding.postImage)
             Glide.with(context).load(item.profilePicture).centerCrop().into(binding.profilePicture)
             binding.profilename.text = item.profileName
+            if (item.postDescription.isEmpty()) {
+                binding.postDescription.hide()
+            } else {
+                binding.postDescription.text = item.postDescription
+            }
             binding.postTimestamp.text = "2 hours ago"
 
         }
